948c3add19eb98e8bdcd54f9103a02f52ddd411c
igorfs10 <igorfs10@gmail.com>
2018-10-22 11:26:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,89 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const config = require('./config')
+﻿//npm install express
+const app = require("express")();
+const http = require("http").createServer(app);
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+//para pegar dados via post
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(bodyParser.json());
+let alunos = [{
+		"id": 1,
+		"nome": "Francisco",
+		"data_nascimento": 20000125,
+		"matricula" : 20858300,
+		"cpf" : "711.301.300-70",
+		"notas": [10.0, 8.2, 7.5]
+		},
+		{
+		"id": 2,
+		"nome": "Juliano",
+		"data_nascimento": 20000920,
+		"matricula" : 20858301,
+		"cpf" : "832.407.200-70",
+		"notas": [9.0, 5.2, 7.5]
+		}
+    ]
+			  
+let ultimoId = 2;
 
-
-const books = [{"id":1, "titulo": "O pequeno principe",
-                "descricao": "Significado do Livro O Pequeno Príncipe O Pequeno Príncipe é uma obra literária do escritor francês Antoine de Saint-Exupéry, que conta a história..."},
-               {"id":2,"titulo": "50 tons de cinza",
-               "descricao": "É a partir do encontro desses dois personagens que se constrói a trama central de Cinquenta Tons de Cinza. Dois arquétipos (no caso, clichês mesmo)"},
-               {"id":3,"titulo": "Kotlin com Android", "descricao": "Neste livro, Kassiano Resende apresenta a linguagem Kotlin tanto para quem já conhece Java ou não, com uma didática prática e divertida."},
-              ]
-
-
-app.get("/" , function(req, res){
-  const help = `
-   <pre>
-     Bem vindo a API de Livros
-     Aqui você encontra os seguintes EndPoints:
-     GET /books -> get all books
-     GET /book/:id -> get one book by Id
-   </pre>
-   `
-
-   res.send(help)
+app.get("/", (req, res) => {
+	
+	const help = `
+<pre>
+	Welcome to the API of SENAI Jandira =D
+</pre>
+`;
+	res.send(help);
 });
 
-app.get("/books", (req, res) => {
-  res.send(books)
+app.post("/novo", (req, res) =>{
+	ultimoId++;
+	const id = ultimoId;
+	const nome = req.body.nome;
+	const data_nascimento = req.body.dtnasc;
+	const matricula = req.body.matricula;
+	const cpf = req.body.cpf;
+	const notas = [];
+	
+	const novoAluno = {id, nome, data_nascimento, matricula, cpf, notas};
+	alunos.push(novoAluno);
+	
+	res.send({ "sucesso": true , "msg" : "Adicionado com sucesso" });
 });
 
-app.get("/book/:id", (req, res) => {
-
-  const book = books.filter( u => u.id == req.params.id  );
-
-  if(book.length > 0)
-      res.send(book[0])
-  else
-      res.send({})
-
+app.get("/aluno/:id", (req, res) => {
+	const aluno = alunos.filter(a => a.id == req.params.id);
+	res.send(aluno);
 });
 
+app.get("/alunos", (req, res) => {
+	res.send(alunos);
+});
 
-http.listen( config.port, function(){
-  console.log(`Servidor rodando na porta ${config.port}`);
+app.get("/deletar/:id", (req, res) =>{
+	alunos = alunos.filter(a => a.id != req.params.id);
+	res.send({ "sucesso": true , "msg" : "Removido com sucesso"});
+});
+
+app.post("/avaliar", (req, res) =>{
+	const id = req.body.id;
+	const nota = parseFloat(req.body.nota);
+	
+	const index = alunos.findIndex(i => i.id == id);
+	alunos[index].notas.push(nota);
+		
+	res.send({ "sucesso": true , "msg" : "Avaliado com sucesso"});
+});
+
+app.get("/media/:id", (req, res) =>{
+	const index = alunos.findIndex(i => i.id == req.params.id);
+	const aluno = alunos[index];
+	//var sum = rockets.reduce( function( prevVal, elem ) {
+    //return prevVal + elem.launches;
+	//}, 0 );
+	res.send({ "id": aluno.id , "media": 8.5 });
+});
+
+http.listen(5001, () =>{
+	console.log("Servidor escutando na porta 5001");
 });
